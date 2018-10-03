24bd230057cab2e3930ab426884cb7997c8d0db0
Andrew Kim <jeesoo2002@gmail.com>
2018-10-02 20:44:27 -0700

    start in nextjs

---
+++ frontend/.next/5.f6a47a33c0d1e0508a9e.hot-update.js
@@ -0,0 +1,62 @@
+webpackHotUpdate(5,{
+
+/***/ "./pages/sell.js":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__("./node_modules/react/index.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
+var _jsxFileName = "/Users/andrew/code/Adv_react/frontend/pages/sell.js";
+
+
+var Sell = function Sell(props) {
+  return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    }
+  }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    }
+  }, "Sell!"), __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(Link, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    }
+  }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("a", {
+    href: "/Home",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    }
+  }, "Home!")));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Sell);
+    (function (Component, route) {
+      if(!Component) return
+      if (false) return
+      module.hot.accept()
+      Component.__route = route
+
+      if (module.hot.status() === 'idle') return
+
+      var components = next.router.components
+      for (var r in components) {
+        if (!components.hasOwnProperty(r)) continue
+
+        if (components[r].Component.__route === route) {
+          next.router.update(r, Component)
+        }
+      }
+    })(typeof __webpack_exports__ !== 'undefined' ? __webpack_exports__.default : (module.exports.default || module.exports), "/sell")
+  
+/* WEBPACK VAR INJECTION */}.call(__webpack_exports__, __webpack_require__("./node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=5.f6a47a33c0d1e0508a9e.hot-update.js.map
