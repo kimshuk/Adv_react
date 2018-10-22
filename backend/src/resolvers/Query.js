315bcb9184a38ce00e2db7b39b44d9dc86740eca
Andrew Kim <jeesoo2002@gmail.com>
2018-10-21 22:19:38 -0700

    need to fix prisma server connection

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,8 +1,8 @@
 const Query = {
-  dogs(parent, arguments, ctx, info) {
-    global.dogs = global.dogs || [];
-    return global.dogs;
-  }
+  //   dogs(parent, arguments, ctx, info) {
+  //     global.dogs = global.dogs || [];
+  //     return global.dogs;
+  //   }
 };
 
 module.exports = Query;
