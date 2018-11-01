3c557b7916e930834d9e99e4439095ff3b489410
developer <jeesoo2002@gmail.com>
2018-11-01 16:58:18 -0700

    ch 20 5:58

--- frontend/.next/static/commons/manifest.js
+++ frontend/.next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "632e40ab06b101246710"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "0647658ad700d3481c15"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
---
+++ frontend/components/DeleteItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+
+const DELETE_ITEM_MUTATION = gql`
+  mutation DELETE_ITEM_MUTATION($id: ID!) {
+    deleteItem(id: $id) {
+      id
+    }
+  }
+`;
+
+class DeleteItem extends Component {
+  render() {
+    return (
+      <Mutation>
+        <button>{this.props.children}</button>;
+      </Mutation>
+    );
+  }
+}
+
+export default DeleteItem;
