9cabff386c55fd00dd24d0c93d63bda1bb0b3468
developer <jeesoo2002@gmail.com>
2018-11-05 14:04:54 -0800

    pagination func added

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -1,8 +1,10 @@
 import React from "react";
 import gql from "graphql-tag";
 import { QUERY, Query } from "react-apollo";
+import Head from "next/head";
+import Link from "next/link";
 import PaginationStyles from "./styles/PaginationStyles";
-
+import { perPage } from "../config";
 const PAGINATION_QUERY = gql`
   query PAGINATION_QUERY {
     itemsConnection {
@@ -14,13 +16,49 @@ const PAGINATION_QUERY = gql`
 `;
 
 const Pagination = props => (
-  <PaginationStyles>
-    <Query query={PAGINATION_QUERY}>
-      {({ data, loading, error }) => (
-        <p>Hi im the pagination {data.itemsConnection.aggregate.count}!</p>
-      )}
-    </Query>
-  </PaginationStyles>
+  <Query query={PAGINATION_QUERY}>
+    {({ data, loading, error }) => {
+      if (loading) return <p>Loading...</p>;
+      const count = data.itemsConnection.aggregate.count;
+      const pages = Math.ceil(count / perPage);
+      const page = props.page;
+      return (
+        <PaginationStyles>
+          <Head>
+            <title>
+              Sick Fits! - Page {page} of {pages}
+            </title>
+          </Head>
+          <Link
+            prefetch
+            href={{
+              pathname: "items",
+              query: { page: page - 1 }
+            }}
+          >
+            <a className="prev" aria-disabled={page <= 1}>
+              Prev
+            </a>
+          </Link>
+          <p>
+            Page {props.page} of {pages}!
+          </p>
+          <p>{count} Items Total</p>
+          <Link
+            prefetch
+            href={{
+              pathname: "items",
+              query: { page: page + 1 }
+            }}
+          >
+            <a className="prev" aria-disabled={page >= pages}>
+              Next
+            </a>
+          </Link>
+        </PaginationStyles>
+      );
+    }}
+  </Query>
 );
 
 export default Pagination;
