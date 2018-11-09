90a5e6eb64698e1b140a029ad3739be02df7ca8e
developer <jeesoo2002@gmail.com>
2018-11-09 14:29:41 -0800

    init create gated sign in

---
+++ frontend/components/PleaseSignin.js
@@ -0,0 +1,7 @@
+import { Query } from "react-apollo";
+import { CURRENT_USER_QUERY } from "./User";
+import { Signin } from "./Signin";
+
+const PleaseSignin = props => <p>Please Sign in!</p>;
+
+export default PleaseSignin;
