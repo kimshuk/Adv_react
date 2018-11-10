5fcb2ab7b3436b575c039a8adf4311bc1a7a00d1
developer <jeesoo2002@gmail.com>
2018-11-09 16:56:21 -0800

    ch 33 3:37

--- frontend/components/PleaseSignin.js
+++ frontend/components/PleaseSignin.js
@@ -2,6 +2,21 @@ import { Query } from "react-apollo";
 import { CURRENT_USER_QUERY } from "./User";
 import { Signin } from "./Signin";
 
-const PleaseSignin = props => <p>Please Sign in!</p>;
+const PleaseSignin = props => (
+  <Query query={CURRENT_USER_QUERY}>
+    {(data, loading) => {
+      if (loading) return <p>Loading...</p>;
+      if (!data.me) {
+        return (
+          <div>
+            <p>Please Sign In before Continuing</p>
+            <Signin />
+          </div>
+        );
+      }
+      return props.children;
+    }}
+  </Query>
+);
 
 export default PleaseSignin;
