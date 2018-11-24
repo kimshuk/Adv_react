2fac233bacdee24a43ea50800a6dc53ecdd60b8a
Andrew Kim <jeesoo2002@gmail.com>
2018-11-23 23:20:09 -0800

    finished ch40

--- frontend/.next/static/commons/manifest.js
+++ frontend/.next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "1e488e4af42abff03052"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "f104cf3c6e197863ae42"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
