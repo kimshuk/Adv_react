384c2fbe7de9355f9f002314befa3127ce857b35
Andrew Kim <jeesoo2002@gmail.com>
2018-11-24 14:49:05 -0800

    pushing before I go ch 43

---
+++ frontend/components/RemoveFromCart.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Mutation } from "react-apollo";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import gql from "graphql-tag";
+import { CURRENT_USER_QUERY } from "./User";
+
+const BigButton = styled.button`
+  font-size: 3rem;
+  background: none;
+  border: 0;
+  &:hover {
+    color: ${props => props.theme.red};
+    cursor: pointer;
+  }
+`;
+
+class RemoveFromCart extends React.Component {
+  render() {
+    return <BigButton title="Delete Item">&times;</BigButton>;
+  }
+}
+
+export default RemoveFromCart;
